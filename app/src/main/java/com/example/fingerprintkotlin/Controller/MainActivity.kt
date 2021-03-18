2dd9a538d3c600eaffa71f09bbcb81ec4cd588ee
Kunlachart Promfang <kunlachart.p@ku.th>
2021-03-18 14:43:00 +0700

    add directory 2

--- app/src/main/java/com/example/fingerprintkotlin/Controller/MainActivity.kt
+++ app/src/main/java/com/example/fingerprintkotlin/Controller/MainActivity.kt
@@ -1,13 +1,14 @@
-package com.example.fingerprintkotlin
+package com.example.fingerprintkotlin.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.fingerprintkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Hekkii","")
+        Log.e("Hekkii","sdsd")
     }
 }
