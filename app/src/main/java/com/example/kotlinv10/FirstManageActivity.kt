ef02276a8e3084498f166e2d7f20b518913dce70
guitar <keeta254211@gmail.com>
2021-03-19 17:15:56 +0700

    add firstmanage activity

---
+++ app/src/main/java/com/example/kotlinv10/FirstManageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinv10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_manage)
+    }
+}
---
+++ app/src/main/java/com/example/kotlinv10/model/SectorAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinv10.model
+
+class SectorAdapter {
+}
